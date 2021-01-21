bfbe88e08e47a6d7e9fd93798033094dc26fd436
Marcelo Pessini <67603422+mpessini@users.noreply.github.com>
2021-01-21 14:32:46 -0300

    Update styles.js

--- src/styles.js
+++ src/styles.js
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 
 export const LoginBody = styled.div`
   align-items: center;
+  background-color: black;
   display: flex;
   flex-direction: column;
   justify-content: center;
