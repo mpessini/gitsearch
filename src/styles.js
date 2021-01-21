c68c7a737f1585c7cb39ba39e823afadf28b1069
Marcelo Pessini <67603422+mpessini@users.noreply.github.com>
2021-01-21 17:15:29 -0300

    Update styles.js

--- src/styles.js
+++ src/styles.js
@@ -14,6 +14,7 @@ export const SearchBody = styled.div`
   align-items: center;
   display: flex;
   flex-direction: column;
+  font-family: sans-serif;
   justify-content: center;
 `;
 
@@ -22,6 +23,7 @@ export const HeaderStyle = styled.header`
   align-items: center;
   display: flex;
   flex-direction: column;
+  font-family: sans-serif;
   justify-content: center;
   width: 80%;
 
